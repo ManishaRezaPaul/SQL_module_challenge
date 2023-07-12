a4bf55c9c14b133645c776bc75b3faa024819190
ManishaRezaPaul <manishareza@gmail.com>
2023-07-12 17:01:18 -0400

    File updated

--- SQL_module_challenge.sql
+++ SQL_module_challenge.sql
@@ -5,26 +5,26 @@ DROP TABLE IF EXISTS title
 -- Creating schemas and importing values for the tables
 
 CREATE TABLE departments (
-  dept_no VARCHAR PRIMARY KEY,
-  dept_name VARCHAR NOT NULL
+  dept_no VARCHAR (10) PRIMARY KEY,
+  dept_name VARCHAR (20) NOT NULL
   );
 
 SELECT * FROM departments
 
 CREATE TABLE titles (
-  title_id VARCHAR PRIMARY KEY,
-  title VARCHAR NOT NULL 
+  title_id VARCHAR (20) PRIMARY KEY,
+  title VARCHAR (20) NOT NULL 
 );
 
 SELECT * FROM titles
 
 CREATE TABLE employees (
-	emp_no INT PRIMARY KEY,
-	emp_title_id VARCHAR NOT NULL,
+	emp_no INT (10) PRIMARY KEY,
+	emp_title_id VARCHAR (20) NOT NULL,
   	birth_date DATE NOT NULL,
-  	first_name VARCHAR NOT NULL,
-  	last_name VARCHAR NOT NULL,
-  	sex VARCHAR NOT NULL,
+  	first_name VARCHAR (50) NOT NULL,
+  	last_name VARCHAR (50) NOT NULL,
+  	sex VARCHAR (10) NOT NULL,
   	hire_date DATE NOT NULL,
 	FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
 );
@@ -32,9 +32,9 @@ CREATE TABLE employees (
 SELECT * FROM employees
 
 CREATE TABLE dept_emp (
-	id VARCHAR PRIMARY KEY,
-	emp_no INT NOT NULL,
-	dept_no VARCHAR NOT NULL,
+	id VARCHAR (20) PRIMARY KEY,
+	emp_no INT (10) NOT NULL,
+	dept_no VARCHAR (10)NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
@@ -42,9 +42,9 @@ CREATE TABLE dept_emp (
 SELECT * FROM dept_emp
 
 CREATE TABLE dept_manager (
-	id VARCHAR PRIMARY KEY,
-	dept_no VARCHAR NOT NULL,
-	emp_no INT NOT NULL,
+	id VARCHAR (20) PRIMARY KEY,
+	dept_no VARCHAR (10) NOT NULL,
+	emp_no INT (10) NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
  );
@@ -52,8 +52,8 @@ CREATE TABLE dept_manager (
 SELECT * FROM dept_manager
 
 CREATE TABLE salaries (
-	emp_no INT PRIMARY KEY,
-	salary INT NOT NULL,
+	emp_no INT (10) PRIMARY KEY,
+	salary INT (10) NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
